fc63e4eb33e3f2d377b42a69b65180a8714a4a06
Leo Scherer <lenoxy@gmx.ch>
2022-09-17 01:23:08 +0200

    Add working ws example

--- hackzurich22-backend/main.py
+++ hackzurich22-backend/main.py
@@ -4,9 +4,29 @@ import asyncio
 from websockets import serve
 
 
-async def echo(websocket):
+async def echo(websocket, path):
     async for message in websocket:
-        await websocket.send(message)
+        if matches_path(path, "/journey"):
+            await websocket.send(path + " " + message)
+        elif matches_path(path, "/lobby"):
+            # I assume we get the lift number from the message
+            return
+        elif matches_path(path, "/elevator"):
+            # I assume we get the lift number from the message
+            return
+        else:
+            print("unknown WS path:" + path)
+
+
+def matches_path(path, str):
+    if path == str:
+        return True
+    elif path + "/" == str:
+        return True
+    elif path == str + "/":
+        return True
+    else:
+        return False
 
 
 async def main():
