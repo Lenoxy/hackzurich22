262b98efa06d9a1a33348c39beb62b1d9be4da75
DawidKapka <kapkadawid6@gmail.com>
2022-09-17 00:11:43 +0200

    create destination-picker and add tailwind

--- hackzurich22-webapp/src/app/app.module.ts
+++ hackzurich22-webapp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DestinationPickerComponent } from './components/destination-picker/destination-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DestinationPickerComponent
   ],
   imports: [
     BrowserModule,
---
+++ hackzurich22-webapp/src/app/components/destination-picker/destination-picker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-picker',
+  templateUrl: './destination-picker.component.html',
+  styleUrls: ['./destination-picker.component.scss']
+})
+export class DestinationPickerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
