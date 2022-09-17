91d6bfdf0f965d68a5a0a5b201fbbd9a3d725549
Zaheer Chothia <zaheer.chothia@gmail.com>
2022-09-17 11:17:05 +0200

    backend: less granular reporting (for pull-style query)

--- backend-proto/lift-status.py
+++ backend-proto/lift-status.py
@@ -2,12 +2,15 @@
 
 import asyncio
 import json
+from pprint import pprint
 
 import websockets
 
 BASE_URL = "wss://hack.myport.guide"
 
-async def handler():
+POSITION_TRACKER = dict()
+
+async def lift_status_handler():
     async with websockets.connect(f'{BASE_URL}/') as websocket:
         payload = json.dumps({
             "Method": "SUBSCRIBE",
@@ -17,16 +20,37 @@ async def handler():
         await websocket.send(payload)
 
         async for message in websocket:
-            print(message)
-
+            #print(message)
             event = json.loads(message)
+
+            # Ignore initial SUB confirmation
+            try:
+                if event['Reason-Phrase'] == 'Created':
+                    continue
+            except KeyError:
+                pass
+
             assert event['type'] == 'topic'
             data = event['data']
 
             # Interesting fields in the response:
-            #   data['floor']: int
             #   data['movingState']: enum[string] (one of: stand-still | starting | moving | landing)
             #   data['doorState']: enum[string] (one of: closing | closed | locking | locked | opening | opened)
-            #   data['target']: string (e.g. "1.1.2")
+
+            liftId = data['target']  # e.g. "1.1.2", uses hierarchical naming to reflect {building}.{group}.{car}
+            floor = data['floor']
+            POSITION_TRACKER[liftId] = floor
+
+async def periodic_log():
+    while True:
+        await asyncio.sleep(5)
+        pprint(POSITION_TRACKER)
+        print()
+
+async def handler():
+    await asyncio.gather(
+        lift_status_handler(),
+        periodic_log(),
+    )
 
 asyncio.run(handler())
