694814ee3afc77faf590e4ab39a01a37698877b9
Leo Scherer <lenoxy@gmx.ch>
2022-09-17 20:06:54 +0200

    Push elevator info on new ride

--- hackzurich22-backend/elevator.py
+++ hackzurich22-backend/elevator.py
@@ -3,17 +3,23 @@ from in_memory_storage import Elevator, Ride, elevators
 
 
 def open_ws(ws, lift_name: str):
-    # on
-    elevator = create_or_return_existing(lift_name)
+    elevator = create_or_return_existing(ws, lift_name)
     print(elevator)
     ws.send(elevator.toJSON())
 
 
-def create_or_return_existing(name: str):
+def create_or_return_existing(ws, name: str):
     for elevator in elevators:
         if elevator.name == name:
             return elevator
 
-    elevator = Elevator(name, "idle", 0, [])
+    elevator = Elevator(ws, name, "idle", 0, [])
     elevators.append(elevator)
     return elevator
+
+
+def getAvailableElevator():
+    # todo implement
+    # for elevator in elevators:
+    #     if elevator.state == "waiting"
+    return elevators[0]
