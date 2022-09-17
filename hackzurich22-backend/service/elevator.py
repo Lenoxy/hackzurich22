b40752d05257f61c7d450049b60617ef079571c2
Leo Scherer <lenoxy@gmx.ch>
2022-09-17 15:25:22 +0200

    Dump elevator

--- hackzurich22-backend/service/elevator.py
+++ hackzurich22-backend/service/elevator.py
@@ -1,6 +1,10 @@
-from ..in_memory_storage import Elevator, Ride
+import json
+
+from ..in_memory_storage import Elevator, Ride, elevators
 
 
 def open_ws(ws, lift_name: str):
     # on
-    ws.send(Elevator(lift_name, "idle", []))
+    elevator = Elevator(lift_name, "idle", [])
+    elevators.append(elevator)
+    ws.send(json.dumps(elevator))
