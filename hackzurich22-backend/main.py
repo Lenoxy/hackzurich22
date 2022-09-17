870a5016b9ec391a345dc3cec0e0a22e9b83d2f2
Leo Scherer <lenoxy@gmx.ch>
2022-09-17 17:41:13 +0200

    Stringify

--- hackzurich22-backend/main.py
+++ hackzurich22-backend/main.py
@@ -16,7 +16,7 @@ sock = Sock(app)
 
 
 @app.route("/ride/new", methods=['POST'])
-def new_session():
+def new_ride():
     ride = request.get_json()
     user_id = in_memory_storage.get_ride_id()
     in_memory_storage.elevators[0].rides.append(
@@ -26,8 +26,7 @@ def new_session():
             ride['to_floor']
         )
     )
-
-    return user_id
+    return str(user_id)
 
 
 
