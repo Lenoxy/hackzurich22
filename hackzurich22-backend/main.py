344a07ab250347422285de260ede7298ef0df9e2
Leo Scherer <lenoxy@gmx.ch>
2022-09-17 04:51:51 +0200

    Dockerize

--- hackzurich22-backend/main.py
+++ hackzurich22-backend/main.py
@@ -9,7 +9,6 @@ from flask import request
 
 from service import smartphone, elevator, lobby
 
-# Port 5000
 app = Flask(__name__)
 sock = Sock(app)
 
@@ -21,15 +20,15 @@ def new_session():
     ride = request.get_json()
     id = str(next(gen))
 
-    in_memory_storage.rides.append(
-        in_memory_storage.Ride(
-            id,
-            ride['from_floor'],
-            ride['to_floor'],
-            ride['room'],
-            ride['patient_name']
-        )
-    )
+    # in_memory_storage.rides.append(
+    #     in_memory_storage.Ride(
+    #         id,
+    #         ride['from_floor'],
+    #         ride['to_floor'],
+    #         ride['room'],
+    #         ride['patient_name']
+    #     )
+    # )
 
     return id
 
---
+++ hackzurich22-backend/readme.md
@@ -0,0 +1,4 @@
+# Docker
+Run with ```docker-compose up -d``` on port 3000
+
+Locally run with ```pip install -r requirements.txt && flask --app main.py run ``` on port 5000
