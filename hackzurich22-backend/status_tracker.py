beaa987b597ec31a166a411894ff527656ea8da4
Zaheer Chothia <zaheer.chothia@gmail.com>
2022-09-18 02:50:41 +0200

    backend: wire up status logic with websockets
    Once again, tihs is fully untested but should in principle now
    notify the frontend about what all the lifts are currently doing.

--- hackzurich22-backend/status_tracker.py
+++ hackzurich22-backend/status_tracker.py
@@ -1,3 +1,5 @@
+import in_memory_storage
+
 import asyncio
 import json
 from pprint import pprint
@@ -6,6 +8,7 @@ import websockets
 
 BASE_URL = "wss://hack.myport.guide"
 
+KNOWN_LIFTS = set()
 POSITION_TRACKER = dict()
 DOOR_STATUS_TRACKER = dict()
 MOVEMENT_TRACKER = dict()
@@ -30,6 +33,7 @@ async def handler():
             except KeyError:
                 pass
 
+            # Extract all relevant fields of interest from the received message.
             assert event['type'] == 'topic'
             data = event['data']
 
@@ -42,9 +46,30 @@ async def handler():
             doorState = data['doorState']
             movement = data['movingState']
 
+            # Store everything we know about each lift keyed by its identifier.
+            KNOWN_LIFTS.add(liftId)
             POSITION_TRACKER[liftId] = floor
             DOOR_STATUS_TRACKER[liftId] = doorState
             MOVEMENT_TRACKER[liftId] = movement
 
-            ## TODO: not yet implemented -> notify everyone downstream of any
-            ## changes to position, floor or door status
+            # Pass along latest changes to downstream consumers. (NOTE: exact
+            # formatting used by the frontend is as yet to be defined.)
+            for lift_id in KNOWN_LIFTS:
+                change_summary = {
+                    position: POSITION_TRACKER[lift_id],
+                    door: DOOR_STATUS_TRACKER[lift_id],
+                    movement: MOVEMENT_TRACKER[lift_id],
+                }
+                digest = json.dumps(change_summary)
+
+                for elev in in_memory_storage.elevators:
+                    # We aren't selective and just flood information about all lifts
+                    # to every nkown smartphones and door signs, irrespective of
+                    # whether this notification would be of interest.
+                    #
+                    # NOTE: if we'd prefer to be more smart, we could use our
+                    # registration list to cross-check which information is relevant
+                    # for which consumer and only send messages selectively.
+                    elev.websocket.send(digest)
+                    for ride in elev.rides:
+                        ride.websocket.send(digest)
